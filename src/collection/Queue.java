ed42e367f630f104c29fcd6f469ab14ff81719bd
Runpop <ffdi@foxmail.com>
2015-10-07 11:04:02 +0800

    增加队列

--- src/collection/Queue.java
+++ src/collection/Queue.java
@@ -2,54 +2,59 @@ package collection;
 
 import java.util.Iterator;
 
-public class Stack<T> implements Iterable<T>{
+public class Queue<T> implements Iterable<T>{
 	
-	/**
-	 * 链表结点
-	 * @author diff
-	 *
-	 */
 	private class Node{
 		T item;
 		Node next;
 	}
 	
-	//栈顶
 	private Node first;
+	private Node last;
 	private int N;
 	
 	public boolean isEmpty(){
-		return first == null;
+		return N == 0;
 	}
 	
 	public int size(){
 		return N;
 	}
 	
-	public void push(T item){
-		Node oldfirst = first;
-		first = new Node();
-		first.item = item;
-		first.next = oldfirst;
+	public void enqueue(T item){
+		//向表尾添加元素
+		Node oldlast = last;
+		last = new Node();
+		last.item = item;
+		if(isEmpty()){
+			first = last;
+		}else{
+			oldlast.next = last;
+		}
 		N++;
-		
 	}
 	
-	public T pop(){
-		T item = first.item;
-		Node next = first.next;
-		first.next = null;
-		first = next;
-		return item;
+	public T dequeue(){
+		//从表头删除元素
+		Node oldfirst = first;
+		first = first.next;
+		oldfirst.next = null;
+		N--;
+		if(isEmpty()){
+			last = null;
+		}
+		return oldfirst.item;
 	}
 
 	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
 		return new ListIterator();
 	}
 	
 	private class ListIterator implements Iterator<T>{
 		private Node current = first;
 		public boolean hasNext() {
+			// TODO Auto-generated method stub
 			return current != null;
 		}
 
@@ -61,8 +66,9 @@ public class Stack<T> implements Iterable<T>{
 		}
 
 		public void remove() {
+			// TODO Auto-generated method stub
 			
 		}
 		
-	}
+	} 
 }
--- src/collection/TestCase.java
+++ src/collection/TestCase.java
@@ -1,13 +1,12 @@
 package collection;
 
 public class TestCase {
-
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
-
+	
 }
